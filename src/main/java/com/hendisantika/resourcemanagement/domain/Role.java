c6f7b4a9663b11143c7738e9a54d34f519882180
hendisantika <hendisantika@yahoo.co.id>
2019-07-24 06:34:40 +0700

    #12 - Add Role Domain

---
+++ src/main/java/com/hendisantika/resourcemanagement/domain/Role.java
@@ -0,0 +1,23 @@
+package com.hendisantika.resourcemanagement.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : resource-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-24
+ * Time: 06:34
+ */
+@Entity(name = "Role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column
+    private String name;
+}
