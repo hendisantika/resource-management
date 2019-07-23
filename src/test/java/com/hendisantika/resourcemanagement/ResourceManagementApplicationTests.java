bf0c7984933788ae4d1462d9b7c41fdbe6545c09
hendisantika <hendisantika@yahoo.co.id>
2019-07-24 06:23:49 +0700

    #1 - Initial project setup

---
+++ src/test/java/com/hendisantika/resourcemanagement/ResourceManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.resourcemanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourceManagementApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
