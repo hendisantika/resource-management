a762f1f50a2872ce60815cbccb7f491568adbeef
hendisantika <hendisantika@yahoo.co.id>
2019-07-24 06:37:21 +0700

    #16 - Modify Member Domain

--- src/main/java/com/hendisantika/resourcemanagement/domain/Member.java
+++ src/main/java/com/hendisantika/resourcemanagement/domain/Member.java
@@ -1,7 +1,6 @@
 package com.hendisantika.resourcemanagement.domain;
 
 import lombok.Data;
-import org.springframework.context.annotation.Role;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,56 +27,64 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     @Column(name = "Name")
     @NotEmpty(message = "{fullname.sk}")
     private String name;
+
     @NotEmpty(message = "Enter {0}")
     @Size(min = 5, max = 8)
     @Column(name = "Username")
     private String username;
+
     @NotEmpty(message = "Enter {0}")
     // @Size(min=5, max=8)
     @Column(name = "Password")
     private String password;
+
     @Transient
     private String retype;
+
     @Column
     @NotEmpty(message = "Select {0}")
     private String gender;
+
     @Email
     @NotEmpty
     @Column(name = "Email")
     private String email;
+
     @Column
     private String imageUrl;
+
     @Transient
     private MultipartFile image;
+
     @NotNull(message = "Valid date dd/MM/yyyy")
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     @Temporal(TemporalType.DATE)
     private Date dob;
+
     @Column
     @NotEmpty(message = "Enter {0}")
     private String address;
+
     // @OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.ALL)
     // private List<Role>lstRoleMember;
     @OneToOne(fetch = FetchType.EAGER)
     private Role role;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "memberItem", cascade = CascadeType.ALL)
     private List<Item> lstMemberItems;
+
     @ManyToOne
     @JoinColumn(name = "Room_Id_Member")
     private Room roomMember;
+
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "memberTranscation", cascade = CascadeType.ALL)
     private List<Transaction> lstTranscationMember;
+
     @Transient
     private String enablePasswordChange;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
