1f5e3f285ab7fa16a46926de699b03e8b4a2ea66
hendisantika <hendisantika@yahoo.co.id>
2019-07-24 06:35:07 +0700

    #13 - Add Response Domain

---
+++ src/main/java/com/hendisantika/resourcemanagement/domain/Response.java
@@ -0,0 +1,18 @@
+package com.hendisantika.resourcemanagement.domain;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : resource-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-24
+ * Time: 06:34
+ */
+@Data
+public class Response {
+    private boolean isSuccess;
+    private String message;
+}
