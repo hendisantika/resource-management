7d9b93da7fef24cc410c67622e83b6b26919b39b
hendisantika <hendisantika@yahoo.co.id>
2019-07-24 06:32:13 +0700

    #10 - Add TransactionStatus Domain

---
+++ src/main/java/com/hendisantika/resourcemanagement/domain/TransactionStatus.java
@@ -0,0 +1,14 @@
+package com.hendisantika.resourcemanagement.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : resource-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-24
+ * Time: 06:31
+ */
+public enum TransactionStatus {
+    ACTIVE, COMPLETED
+}
