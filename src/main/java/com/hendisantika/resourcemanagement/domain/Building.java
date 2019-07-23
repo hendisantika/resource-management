b3c25ebfdb63cd724b1134e4eb62415448aead5a
hendisantika <hendisantika@yahoo.co.id>
2019-07-24 06:30:44 +0700

    #8 - Add Building Domain

---
+++ src/main/java/com/hendisantika/resourcemanagement/domain/Building.java
@@ -0,0 +1,33 @@
+package com.hendisantika.resourcemanagement.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : resource-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-07-24
+ * Time: 06:30
+ */
+@Entity
+@Data
+public class Building {
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "building", cascade = CascadeType.ALL)
+    List<Room> lstRoomInBuilding;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column
+    @NotEmpty(message = "building name cannot be empty")
+    private String name;
+    @Column
+    @NotEmpty(message = "building desc cannot be empty")
+    private String description;
+
+}
